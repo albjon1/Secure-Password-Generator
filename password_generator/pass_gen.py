41180a632440b942cb8e7d2e240c03c75c21fd2d
Albjon Husha <81479352+albjon1@users.noreply.github.com>
2022-03-19 15:17:09 +0000

    Add files via upload

--- password_generator/pass_gen.py
+++ password_generator/pass_gen.py
@@ -8,6 +8,7 @@ window.title("Secure Password Generator V1.0")
 window.geometry('570x300')
 window.configure(bg='grey12')
 window.iconphoto(False, tk.PhotoImage(file='icon.png'))
+window.resizable(width=False, height=False)
 
 random_chars = '''52EMmOx#l6C3GUTi@DwAJ9g1H#@7*YbNyX*j
 0k8zqhosva_RFWZfPcdKerLS4up_IBnQtV'''
@@ -16,15 +17,15 @@ generated_pass_list = []
 
 def generate_pass():
     pass_input_len = int(input_box.get())
-    if pass_input_len >= 10 and pass_input_len <= 100:
+    if pass_input_len >= 10 and pass_input_len <= 40:
         for i in range(pass_input_len):
             list_choice = random.choice(random_chars)
             generated_pass_list.append(list_choice)
         final_pass = ''.join(generated_pass_list)
         pass_lbl.config(text=f'Password: {final_pass}')
-    elif pass_input_len > 100:
+    elif pass_input_len > 40:
         ct.windll.user32.MessageBoxW(
-         0, 'Maximum password length is 100', 'Error Raised', 0)
+         0, 'Maximum password length is 40', 'Error Raised', 0)
     elif pass_input_len < 10:
         ct.windll.user32.MessageBoxW(
          0, 'Password length must be 10 or more', 'Error Raised', 0)
@@ -42,43 +43,57 @@ def creator(message):
 print(creator('built by albjon V1.0'))
 
 # Enter Password Length Label
-title_lbl = tk.Label(window,
-                     bg='grey12',
-                     fg='#C8C8C8',
-                     text='Enter Password Length',
-                     font='consolas')
+title_lbl = tk.Label(
+ window,
+ bg='grey12',
+ fg='#C8C8C8',
+ text='Enter Password Length',
+ font='consolas')
 title_lbl.pack()
 
 
 # Password Length Entry Widget
-input_box = tk.Entry(window,
-                     bg='grey12',
-                     fg='#C8C8C8',
-                     selectbackground='#C8C8C8',
-                     insertbackground='#C8C8C8')
+input_box = tk.Entry(
+ window,
+ bg='grey12',
+ fg='#C8C8C8',
+ selectbackground='#C8C8C8',
+ insertbackground='#C8C8C8')
 input_box.pack(pady=20)
 
 # Generate Button
-generate_button = tk.Button(window,
-                            text="Generate",
-                            command=generate_pass,
-                            bg='grey12',
-                            fg='#C8C8C8')
+generate_button = tk.Button(
+ window,
+ text="Generate",
+ command=generate_pass,
+ bg='grey12',
+ fg='#C8C8C8')
 generate_button.pack(pady=5)
 
 # Clear Button
-clear_button = tk.Button(window,
-                         text="Clear",
-                         command=clear_password,
-                         bg='grey12',
-                         fg='#C8C8C8')
+clear_button = tk.Button(
+ window,
+ text="Clear",
+ command=clear_password,
+ bg='grey12',
+ fg='#C8C8C8')
 clear_button.pack(pady=5)
 
 # Generated Password Label
-pass_lbl = tk.Label(window,
-                    bg='grey12',
-                    fg='#C8C8C8',
-                    text=generated_pass_list,
-                    font='consolas')
+pass_lbl = tk.Label(
+ window,
+ bg='grey12',
+ fg='#C8C8C8',
+ text=generated_pass_list,
+ font='consolas')
 pass_lbl.pack(pady=5)
+
+ver_lbl = tk.Label(
+ window, bg='grey12',
+ fg='#C8C8C8',
+ text='V1.0',
+ font='consolas')
+ver_lbl.pack(anchor='sw', side='bottom')
+
+
 window.mainloop()
